345079d4bb1e9bc67277613711f14a265b8e076b
ssssseph <122000994+ssssseph@users.noreply.github.com>
2023-07-05 13:36:49 +0900

    first commit

--- src/main/java/com/study/board/Service/BoardService.java
+++ src/main/java/com/study/board/Service/BoardService.java
@@ -3,20 +3,35 @@ package com.study.board.Service;
 import com.study.board.entity.Board;
 import com.study.board.repository.BoardRepositoty;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 public class BoardService {
 
+    
     @Autowired
     private BoardRepositoty boardRepositoty;
+    
+    //글 작성 처리
     public void write(Board board) {
         boardRepositoty.save(board);
     }
 
-    public List<Board> boardList() {
-        return boardRepositoty.findAll();
+    // 게시글 리크트 처리
+    public Page<Board> boardList(Pageable pageable) {
+        return boardRepositoty.findAll(pageable);
+    }
+    
+    //특정 게시글 불러오기
+    public Board boardView(Integer id) {
+        return boardRepositoty.findById(id).get();
+    }
+
+    //특정 게시글 삭제
+    public void boardDelete(Integer id) {
+        boardRepositoty.deleteById(id);
     }
 }
--- src/main/java/com/study/board/controller/BoardContorller.java
+++ src/main/java/com/study/board/controller/BoardContorller.java
@@ -3,10 +3,16 @@ package com.study.board.controller;
 import com.study.board.Service.BoardService;
 import com.study.board.entity.Board;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class BoardContorller {
@@ -20,20 +26,55 @@ public class BoardContorller {
     }
 
     @PostMapping("/board/writepro")
-    public String boardWritePro(Board board) {
+    public String boardWritePro(Board board, Model model, MultipartFile file) throws Exception {
         boardService.write(board);
-        return "";
+        return "redirect:/board/list";
     }
 
     @GetMapping("/board/list")
-    public String boarList(Model model) {
-        model.addAttribute("list", boardService.boardList());
+    public String boarList(Model model, @PageableDefault(page=0, size=10, sort="id", direction = Sort.Direction.DESC) Pageable pageable) {
 
+        Page<Board> list= boardService.boardList(pageable);
+
+        int nowPage = list.getPageable().getPageNumber() + 1;
+        int startPage = Math.max(nowPage -4, 1);
+        int endPage = Math.min(nowPage+5, list.getTotalPages());
+        
+        model.addAttribute("list", list);
+        model.addAttribute("nowPage", nowPage);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        
         return "boardlist";
     }
 
-    @GetMapping("/board/view")
-    public String boardview() {
+    @GetMapping("/board/view") //localhost:8080/board/view?id=1
+    public String boardview(Model model, Integer id) {
+        model.addAttribute("board", boardService.boardView(id));
         return "boardview";
     }
-}
+
+    @GetMapping("/board/delete")
+    public String boardDelete(Integer id) {
+        boardService.boardDelete(id);
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/board/modify/{id}")
+    public String boardModify(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("board", boardService.boardView(id));
+        return "boardmodify";
+    }
+
+    @PostMapping("/board/update/{id}")
+    public String boardUpdate(@PathVariable("id") Integer id, Board board, MultipartFile file) throws Exception{
+
+        Board boardTemp = boardService.boardView(id);
+        boardTemp.setTitle(board.getTitle());
+        boardTemp.setContent(board.getContent());
+
+        boardService.write(boardTemp);
+
+        return "redirect:/board/list";
+        }
+    }
